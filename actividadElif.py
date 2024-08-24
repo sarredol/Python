57cb61e6153f6d33664f101c64f20a7c9561e599
Sebastian Arredondo <sarredondoloa@cesde.net>
2024-08-23 20:44:21 -0500

    Tarea Fundamentos
    Finalizada

--- actividadElif.py
+++ actividadElif.py
@@ -12,7 +12,7 @@ elif current_year < another_year:
     
 # Punto 2
 
-figura = input ("Seleccione la figura que desea calcular: 1: Triangulo, 2: Circulo, 3: Cuadrado, 4: Rectangulo `\n`")
+"""figura = input ("Seleccione la figura que desea calcular: 1: Triangulo, 2: Circulo, 3: Cuadrado, 4: Rectangulo `\n`")
 
 
 if figura == "1":
@@ -36,4 +36,72 @@ elif figura == "4":
     area = base * altura
     print(f"El area del Rectangulo es {area}")
 else:
-    print("Selecciones una opcion valida")
+    print("Selecciones una opcion valida")"""
+
+# Punto 3
+
+"""num1 = int(input("Ingrese el número 1: "))
+num2 = int(input("Ingrese el número 2: "))
+num3 = int(input("Ingrese el número 3: "))
+num4 = int(input("Ingrese el número 4: "))
+
+numeros = [num1, num2, num3, num4]
+
+if num1 == num2 == num3 == num4:
+    print("Todos los números son iguales.")
+
+# Verificar si todos los números son diferentes
+elif len(set(numeros)) == 4:
+    print("Todos los números son diferentes.")
+
+# Verificar si hay al menos un número repetido
+else:
+    print("Hay un número repetido al menos 2 veces.")"""
+
+# Punto 4
+
+"""edad = int(input("Digite su edad: "))
+ingresos = float(input("Digite sus ingresos mensuales: "))
+
+if edad > 18 and ingresos >= 2500000:
+    print("Tiene que tributar")
+else:
+    print("No tiene que tributar")"""
+
+
+# Punto 5
+
+menu = input("Digite operacion a realizar:\n" 
+             "1: suma \n"
+             "2: resta \n"
+             "3: multiplicacion \n"
+             "4: Division \n"
+             "5: potenciacion\n"
+             "6: resto\n")
+
+if menu == "1":
+    num= int (input("Digite el primer numero: "))
+    num += int (input("Digite el segundo numero: "))
+    print(f"El resultado es {num}")
+elif menu == "2":
+    num= int (input("Digite el primer numero: "))
+    num -= int (input("Digite el segundo numero: "))
+    print(f"El resultado es {num}")
+elif menu == "3":
+    num= int (input("Digite el primer numero: "))
+    num *= int (input("Digite el segundo numero: "))
+    print(f"El resultado es {num}")
+elif menu == "4":
+    num= int (input("Digite el primer numero: "))
+    num /= int (input("Digite el segundo numero: "))
+    print(f"El resultado es {num}")
+elif menu == "5":
+    num= int (input("Digite el primer numero: "))
+    num **= int (input("Digite el segundo numero: "))
+    print(f"El resultado es {num}")
+elif menu == "6":
+    num= int (input("Digite el primer numero: "))
+    num %= int (input("Digite el segundo numero: "))
+    print(f"El resultado es {num}")
+else:
+    print("Ingrese una opción válida")
